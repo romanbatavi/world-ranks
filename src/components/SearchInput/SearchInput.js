7e9a76fa72217ed67281b9bb95d5abbbb0b14017
romanbatavi <romanbatavi98@gmail.com>
2021-06-01 15:25:22 +0700

    layout, search

---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,5 @@
+const SearchInput = () => {
+  return <div>
+    <input />
+  </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,22 @@ import Image from 'next/image'
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <Layout>Main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>
+    <div className={styles.counts}>Found {countries.length} countries</div>
+
+    <SearchInput />
+  </Layout>;
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+  
+  return {
+    props: {
+      countries,
+    },
+  };
+};
