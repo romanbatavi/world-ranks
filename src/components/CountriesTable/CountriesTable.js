b9ef1b65e89835b4d2bb7fa74a9e436810d94fd4
romanbatavi <romanbatavi98@gmail.com>
2021-06-02 01:15:48 +0700

    create default desc sort for countries

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,11 +1,18 @@
 import styles from './CountriesTable.module.css'
 
-const orderBy = (countries) => {
-  return countries.sort((a,b) => (a.population > b.population ? 1 : -1));
+const orderBy = (countries, direction) => {
+  if (direction === 'asc') {
+    return [...countries].sort((a,b) => (a.population > b.population ? 1 : -1));
+  }
+  if (direction === 'desc') {
+    return [...countries].sort((a,b) => (a.population > b.population ? -1 : 1));
+  };
+
+  return countries;
 };
 
 const CountriesTable = ({ countries }) => {
-  const orderedCountries = orderBy(countries);
+  const orderedCountries = orderBy(countries, 'desc');
   return ( 
   <div>
     <div className={styles.heading}>
@@ -18,7 +25,7 @@ const CountriesTable = ({ countries }) => {
       </button>
     </div>
 
-    {countries.map((country) => (
+    {orderedCountries.map((country) => (
       <div className={styles.row}>
         <div className={styles.name}>{country.name}</div>
 
